72cd8bf230991cf0cdbed05c23c994b1d4cda510
TJGorrie <tgorri@essex.ac.uk>
2019-11-07 11:34:45 +0000

    Fixing broken function in geotogds

--- R/GEOtoGDS.R
+++ R/GEOtoGDS.R
@@ -9,28 +9,27 @@ getpheno <- function(geo){
 }
 
 geotogds <- function(geo, gds=NULL, method = "wget", keepidat = F, keeptar = F, ...){
-    if(is.null(gds)) gds <- paste0(geo, '.gds')
+    if(is.null(gds)) gds <- sprintf('%s.gds', geo)
     if(!(grepl(x = geo, pattern='.tar.gz'))){
-        url <- paste0("https://www.ncbi.nlm.nih.gov/geo/download/?acc=", geo, "&format=file")
-        tarfile <- paste0("./", geo,".tar.gz")
-        message('Downloading \"', geo, '\" to \"', tarfile,'\".')
+        url <- sprintf("https://www.ncbi.nlm.nih.gov/geo/download/?acc=%s&format=file", geo)
+        tarfile <- sprintf(("./%s.tar.gz", geo)
+        message('Downloading %s to %s', geo, tarfile)
         download.file(url, destfile = tarfile, method = method, quiet = FALSE)
-        dest <- paste0("./", geo)
     } else {
         tarfile <- geo
-        geo <- strsplit(x=geo, split=".tar.gz")[[1]][1]
-        dest <- paste0("./", geo)
+        geo <- gsub('tar.gz', '', geo)
     }
-    message('Unpacking file to \'', geo,'/\'')
-    untar(tarfile, exdir = dest, compressed = 'gzip')
+    dest <- sprintf('./%s', geo)
+    message(sprintf('Unpacking file to %s', geo))
+    untar(tarfile, exdir = dest)
     message('Enumerating .idats!')
     barsgz <- dir(dest, recursive = TRUE)[grepl(".idat.gz", dir(dest, recursive = TRUE))]
-    message('Unpacking ', length(barsgz), " .idats!")
+    message('Unpacking %s .idat files', length(barsgz))
     for(z in barsgz){
         GEOquery::gunzip(filename = paste0(dest, "/", z), destname = paste0(dest, "/", gsub(x = z, pattern = ".gz", replacement='')), remove = TRUE)
     }
     # if(tidy){
-    message('Removing unpacked files!')
+    message('Removing packed files!')
     leftovers <- dir(dest, recursive = TRUE)[grepl(".gz", dir(dest, recursive = TRUE))]
     file.remove(paste0(dest,'/',leftovers))
     # }
